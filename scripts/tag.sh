1faac87972fd14b5ffc2da944b2f4815368492fe
Paul Decoursey <paul@decoursey.net>
2018-10-23 21:20:22 -0500

    explicit tag

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -2,6 +2,6 @@
 
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
-git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
+git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER" $TRAVIS_COMMIT
 git push origin $GIT_TAG
 
