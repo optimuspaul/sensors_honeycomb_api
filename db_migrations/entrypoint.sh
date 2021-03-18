b0ed21d3bdd89bd06dcbfccc7b521b4913c770ff
Ben Talberg <ben.talberg@gmail.com>
2021-03-18 15:04:45 -0500

    run db migration with db_migrations

--- db_migrations/entrypoint.sh
+++ db_migrations/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # python /scripts/20190812-0001.py
-python /scripts/20191129-0001.py
-
+# python /scripts/20191129-0001.py
+cat scripts/20210318-0001.sql | PGPASSWORD=${POSTGRES_PASSWORD} psql -U ${POSTGRES_USER} -d ${POSTGRES_DB} -h ${PGHOST}
 exec $@
